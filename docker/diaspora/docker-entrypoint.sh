36ec20d29d25ab14ae12874ba6c37193805fbb26
Brad Koehn <brad@koehn.com>
2016-07-08 08:07:55 -0500

    added commands to perform basic functions; removed unneeded files

--- docker/diaspora/docker-entrypoint.sh
+++ docker/diaspora/docker-entrypoint.sh
@@ -23,7 +23,7 @@ function run {
 
 function bundle {
 	do_as_diaspora "gem install bundler"
-	do_as_diaspora "bin/bundle install --without test development" ||	do_as_diaspora "bin/bundle install --without test development"
+	do_as_diaspora "bin/bundle install -j4 --without test development" ||	do_as_diaspora "bin/bundle install --without test development"
 }
 
 function init_db {
