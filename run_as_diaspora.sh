8130e9ce3460935e1dce9ff48ec55d6d31eed7e4
Brad Koehn <brad@koehn.com>
2018-03-18 20:50:38 -0500

    build with no-cache and changed how we get the GPG key since the other way didn't work

--- run_as_diaspora.sh
+++ run_as_diaspora.sh
@@ -6,7 +6,8 @@ cd /home/diaspora
 
 umask 000
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+
 curl -L https://s.diaspora.software/1t | bash
 
 echo '[[ -s "/home/diaspora/.rvm/scripts/rvm" ]] && source "/home/diaspora/.rvm/scripts/rvm"' >> /home/diaspora/.bashrc
